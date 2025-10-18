a138ee824e7ca4d4f3cd922e46a94aa19d505f22
Taulant Ndreka <tndreka@2-B-2.42heilbronn.de>
2025-10-18 07:16:03 +0200

    ADD: copy constructor

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,11 +6,14 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 05:07:03 by tndreka           #+#    #+#             */
-/*   Updated: 2025/10/18 07:13:08 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/10/18 07:15:22 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
 Span::Span() : sp(), size(0), count(0)
-{} 
+{}
+
+Span::Span(const Span& other): sp(other.sp), size(other.size), count(other.count)
+{}
