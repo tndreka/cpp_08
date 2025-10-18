9f4733a2236c119ba735170a04e49b90513c6ed8
Taulant Ndreka <tndreka@2-B-2.42heilbronn.de>
2025-10-18 02:58:09 +0200

    ADD: added the exception match not found so it will throw an exception if in my container no occurrence is found through the iteration

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 16:06:14 by tndreka           #+#    #+#             */
-/*   Updated: 2025/10/18 02:22:08 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/10/18 02:56:21 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,30 @@
 
 #include <iostream>
 #include <exception>
+#include <vector> // for vector container
+
+class Nomatch:public std::exception
+{
+    const char* what() noexcept
+    {
+        return "No match found! ";
+    }
+};
+
+
 
 template<typename T>
-
+typename T::iterator easyfind(T& container, int value)
+{
+    try
+    {
+        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+}
 
 #endif
