81bb5adcb85cb16b103a8e6957cf3ee505606bd4
Taulant Ndreka <tndreka@2-B-2.42heilbronn.de>
2025-10-18 07:11:41 +0200

    ADD:test

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 04:58:21 by tndreka           #+#    #+#             */
-/*   Updated: 2025/10/18 04:59:18 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/10/18 07:11:21 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,38 @@
 #define SPAN_HPP
 
 #include <iostream>
+#include <exception>
+#include <vector>
 
 class Span
 {
 private:
-    
+    std::vector<int> sp;
+    unsigned int size;
+    unsigned int count;
 public:
     Span();
+    Span(unsigned int n);
+    Span(const Span& other);
+    Span& operator=(const Span& other);
     ~Span();
+    //Exception cases
+    class Outofrange:public std::exception
+    {
+      const char* what()const noexcept;  
+    };
+    class NoNumbers:public std::exception
+    {
+      const char* what() const noexcept;  
+    };
+    class NoSpanFounded:public std::exception
+    {
+      const char* what() const noexcept;  
+    };
+    class OneNumber:public std::exception
+    {
+      const char* what() const noexcept;  
+    };
 };
-
-
 
 #endif
