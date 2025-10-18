8a1a7800a692b48ac3253859c2b6fcff0672f825
Taulant Ndreka <tndreka@2-B-2.42heilbronn.de>
2025-10-18 02:41:22 +0200

    ADD: easyfind()

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 16:06:14 by tndreka           #+#    #+#             */
+/*   Updated: 2025/10/18 02:22:08 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <exception>
+
+template<typename T>
+
+
+#endif
