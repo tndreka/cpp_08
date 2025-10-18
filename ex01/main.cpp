cb4ea52aad076fddb8c61941872141ef99a9dfad
Taulant Ndreka <tndreka@2-B-2.42heilbronn.de>
2025-10-18 05:02:12 +0200

    ADD: added the subject test case in main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,29 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 04:57:39 by tndreka           #+#    #+#             */
-/*   Updated: 2025/10/18 04:57:40 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/10/18 05:01:43 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+//Subject int main
+int main()
+{
+    Span sp = Span(5);
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+    return 0;
+}
+/*
+    OUTPUT:
+    $> ./ex01
+    2
+    14
+    $>
+*/
