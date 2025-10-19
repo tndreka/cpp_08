683e31c8acc5d31bfa47d6b4c7727b1286415a14
Taulant Ndreka <tndreka@3-H-5.42heilbronn.de>
2025-10-19 19:03:44 +0200

    ADD: implemented canonnical form for the template class mutantstack

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,11 +6,26 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 11:04:01 by tndreka           #+#    #+#             */
-/*   Updated: 2025/10/18 11:04:45 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/10/19 19:02:54 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iostream>
+#include <deque>
+
+template <typename T, class Container = std::deque<T>>
+class MutantStack:public std::stack<T, Container>
+{
+    
+    public:
+    MutantStack();
+    MutantStack(const MutantStack& other);   
+    MutantStack& operator=(const MutantStack& other);
+    ~MutantStack();
+};
 
 #endif
