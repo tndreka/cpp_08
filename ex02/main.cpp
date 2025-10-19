768a56989fdc88a04ba2dafffb6bf981a6b32987
Taulant Ndreka <tndreka@3-H-5.42heilbronn.de>
2025-10-19 19:39:13 +0200

    ADD: subject test case

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,38 @@
 /*   By: tndreka <tndreka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 11:03:28 by tndreka           #+#    #+#             */
-/*   Updated: 2025/10/19 18:20:11 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/10/19 19:37:54 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
+
 int main()
 {
+    std::cout << "======== SUBJECT TEST CASE ===============\n";
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    std::cout << "======== TEST CASE 1 ===============\n";
+    
     return 0;
 }
